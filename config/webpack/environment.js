a6e632c9de62112fa4758feb06036a433caff5a5
bachdx <bachdx.hut@gmail.com>
2020-08-11 11:32:18 +0700

    init js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,19 @@
-const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
+const { environment } = require("@rails/webpacker");
+const { VueLoaderPlugin } = require("vue-loader");
+const vue = require("./loaders/vue");
 
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
-module.exports = environment
+const path = require("path");
+
+const resolver = {
+  resolve: {
+    alias: {
+      vue$: "vue/dist/vue.esm.js",
+      "@": path.resolve(__dirname, "..", "..", "app/javascript/src"),
+    },
+  },
+};
+environment.config.merge(resolver);
+
+environment.plugins.prepend("VueLoaderPlugin", new VueLoaderPlugin());
+environment.loaders.prepend("vue", vue);
+module.exports = environment;
