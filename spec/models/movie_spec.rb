95b11147fc927ca12749e6b77c3453211f401cf0
bachdx <bachdx.hut@gmail.com>
2020-08-18 10:44:20 +0700

    update movie spec

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -7,4 +7,32 @@ RSpec.describe Movie, type: :model do
       expect(association.macro).to eq :belongs_to
     end
   end
+
+  describe "Validations" do
+    subject { FactoryBot.create :user }
+
+    it "is valid with valid attributes" do
+      is_expected.to be_valid
+    end
+
+    it "has one after adding one" do
+      instance_double("Movie")
+    end
+    it "is not valid without youtube_video_id" do
+      subject.youtube_video_id = nil
+      is_expected.to_not be_valid
+    end
+  end
+
+  describe "Methods" do
+    context "update_counter" do
+      it "do not update like" do
+        it "when rate history not created " do
+          movie = FactoryBot.create(:movie)
+
+
+        end
+      end
+    end
+  end
 end
