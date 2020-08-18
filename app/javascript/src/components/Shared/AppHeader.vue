ebeded2c6490248b1acc1976277bc0156918d244
bachdx <bachdx.hut@gmail.com>
2020-08-18 17:05:49 +0700

    header logo

--- app/javascript/src/components/Shared/AppHeader.vue
+++ app/javascript/src/components/Shared/AppHeader.vue
@@ -1,7 +1,32 @@
 <template>
   <div class="app-header">
-    <a v-if="loggedIn" @click="executeLogout()">Logout</a>
-    <a v-else @click="showModal()">Login</a>
+    <div class="box-left">
+      <div class="logo"></div>
+    </div>
+    <div class="box-right">
+      <ul class="menu" v-if="loggedIn">
+        <li>
+          <span>
+            <small>Hi</small>
+            {{ username }}
+          </span>
+          <ul class="dropdown-menu">
+            <li>
+              <a href="/uploaded">My Movies</a>
+            </li>
+            <li>
+              <a href @click="executeLogout()">Logout</a>
+            </li>
+          </ul>
+        </li>
+        <li class="upload">
+          <a @click.prevent="showUploadModal()">Upload Movie</a>
+        </li>
+      </ul>
+      <ul class="menu" v-else>
+        <li @click="showModal()">Login/Register</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -25,6 +50,7 @@ export default {
     showModal() {
       this.$root.$refs.loginModal.show();
     },
+    showUploadModal() {},
   },
 };
 </script>
